3f47f7608e0479ea4b0e360f8b2e5f4c2842097a
Sandy Zhang <sandyzhang142@gmail.com>
2022-11-04 13:15:40 -0400

    commit tree a18b1e3337d6843e11c4e50798505b144dd6b12f

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-function addingEventListener() {
+function addingEventListener(){ 
+    const input = document.getElementById('button');
+    function clickAlert() {
+        alert('I was clicked!');
+      }
+      
+      input.addEventListener('click', clickAlert);
+
 }
+
+// const input = document.querySelector('#button');
+// function clickAlert() {
+//     alert('I was not clicked!')
+// }
+// input.addEventListener('click', clickAlert);
